46fbf7d5cf4b82b57bc6b5ff2a127036444ac76d
amne <amentewodros4@gmail.com>
2021-10-17 13:03:41 +0300

    still not working

--- index.js
+++ index.js
@@ -64,10 +64,10 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
 	}
 });
 
-app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+	res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 app.get("/", (req, res) => {
